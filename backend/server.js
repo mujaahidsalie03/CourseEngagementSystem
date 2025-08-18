45d1c167168701e8a3b10e760aaf48ac5aaedf48
mujaahidsalie03 <slxmuj002@myuct.ac.za>
2025-08-18 15:34:05 +0200

    Backend basic setup complete

---
+++ backend/server.js
@@ -0,0 +1,28 @@
+// server.js
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Testing the route
+app.get('/', (req, res) => {
+  res.send('Hello from backend!');
+});
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
