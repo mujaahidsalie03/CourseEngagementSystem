3892cb61833a81f2f8ce599e3afd3f1557a16f92
suvarnchetty <suvarnchetty123@gmail.com>
2025-08-19 20:57:44 +0200

    Create Course.js
    Added the Course model with fields courseName, courseCode, lecturerID, students

---
+++ backend/models/Course.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const courseSchema = new Schema({
+    courseName: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    courseCode: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lecturerID: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    students: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+} ,{timestamps: true});
+
+module.exports = mongoose.model('Course', courseSchema);
