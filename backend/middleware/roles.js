f27d84f1a59e5a7b816f72cc0f6ab9fcd37819e3
Somendra Appadoo <appadoosomendra10@gmail.com>
2025-09-17 16:03:35 +0200

    Copied backend folder from friend's branch

--- backend/middleware/roles.js
+++
@@ -1,7 +0,0 @@
-// middleware/roles.js
-module.exports = function requireRole(role) {
-  return (req, res, next) => {
-    if (req.user && req.user.role === role) return next();
-    return res.status(403).json({ message: 'Forbidden' });
-  };
-};
